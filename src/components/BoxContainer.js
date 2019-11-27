b1d606815b0b975daea7bba63e29eea1c9311a55
Oliver Sun <olisun@mac.com>
2019-11-27 14:21:58 -0800

    pushing up css components corresponding to their js components

--- src/components/BoxContainer.js
+++ src/components/BoxContainer.js
@@ -1,31 +1,27 @@
 import React, { Component } from 'react';
 import { WOW } from 'wowjs/dist/wow';
 import Box from './Box';
+import './BoxContainer.css';
 
 class BoxContainer extends Component {
+  static defaultProps = {
+    numberOfBoxes: 24,
+  }
 
   componentDidMount() {
     new WOW().init()
   }
 
   render() {
+    const { numberOfBoxes } = this.props;
+    // Array.from turns numberOfBoxes from an object to an array so we can map 18 boxes.
+    const boxes = Array.from({ length: numberOfBoxes }).map(() => (<Box />));
     return (
       <div className="wow zoomIn slow">
-        <div className="styleOne">
-          <Box />
-          <Box />
-          <Box />
-          <Box />
-          <Box />
-          <Box />
-          <Box />
-          <Box />
-          <Box />
-          <Box />
-          <Box />
-          <Box />
-          <Box />
-          <Box />
+        <div className="styleOne">  
+          <div className="BoxContainer">
+            {boxes}
+          </div>
         </div>
       </div>
 
